6d67c32216376dda2f5f08df7eab981856ed8a35
TheGreatCalendario <TheGreatCalendario@users.noreply.github.com>
2026-07-24 09:27:09 +0000

    deploy: f19d00dfc7384b843ef0f642e31cb6ef3377583b

---
+++ scripts/controllers/about.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name therapiepapstApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the therapiepapstApp
+ */
+angular.module('therapiepapstApp')
+  .controller('AboutCtrl', function () {
+    this.model = {
+      ustid: 'AT-XXX'
+    };
+  });
